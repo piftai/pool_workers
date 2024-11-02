15964e5ea58852c3fa7dc78704403a5612b1f884
piftai <piftaiwork@gmail.com>
2024-11-02 15:57:00 +0300

    refactoring

--- worker_pool/pool.go
+++ worker_pool/pool.go
@@ -2,7 +2,6 @@ package worker_pool
 
 import (
 	"fmt"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -62,42 +61,4 @@ func (p *WorkerPool) StopWorker(id int) {
 			break
 		}
 	}
-}
-
-func main() {
-	workerPool := NewWorkerPool()
-
-	// Стартуем несколько воркеров
-	workerPool.StartWorker(1)
-	time.Sleep(time.Second * 2)
-	workerPool.StartWorker(2)
-	workerPool.StartWorker(3)
-	workerPool.StartWorker(4)
-	workerPool.StartWorker(5)
-
-	// Добавляем задачи
-	for i := 0; i < 100; i++ {
-		workerPool.TaskChan <- "task №" + strconv.Itoa(i)
-	}
-	time.Sleep(time.Second)
-
-	// Динамически добавляем воркера
-	workerPool.StartWorker(3)
-	for i := 100; i < 150; i++ {
-		workerPool.TaskChan <- "task №" + strconv.Itoa(i)
-	}
-
-	// Динамически удаляем воркера
-	workerPool.StopWorker(1)
-	fmt.Println("Worker №1 was deleted.")
-
-	time.Sleep(time.Second * 10)
-
-	// Завершаем оставшиеся задачи
-	for i := 150; i < 1000; i++ {
-		workerPool.TaskChan <- "task №" + strconv.Itoa(i)
-	}
-
-	time.Sleep(time.Second)
-	close(workerPool.TaskChan)
 }
